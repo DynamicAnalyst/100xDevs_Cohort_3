a5cac228e44351bfe234eefe8354e3bced57a3b1
DynamicAnalyst <207145253+DynamicAnalyst@users.noreply.github.com>
2025-04-15 22:21:44 +0530

    Initial push

---
+++ (5.0.2) javascriptPractice_cohort3_(3.1_DOM(simple).js
@@ -0,0 +1,15 @@
+// code connection ref. 1
+
+
+// let counter = 0;
+
+// function timerCallback(){
+//     // console.log(counter);
+//     document.querySelectorAll("h2")[1].innerHTML = counter; // you can also write ...const el = document.querySelectorAll("h2")[1] ..in next line ...// el.innerHTML = counter                                                                                
+//     counter = counter + 1;
+// }
+
+// setInterval(timerCallback, 1000);
+
+// // =========================================================================================
+
